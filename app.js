68e48f839f74c96a57215df0de5ce2b7ff3085d0
Sagar Vadalia <sv344@cornell.edu>
2019-06-20 11:48:44 -0400

    get / working

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + 'public'));
+app.get('/', (req, res) => {
+  res.send('hello');
+})
+const PORT = 1000;
+app.listen(PORT, () => {
+  console.log('App listening');
+})
