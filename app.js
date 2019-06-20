5bb7a05212837964e0f5a7b857a5afca93e2a119
Sagar Vadalia <sv344@cornell.edu>
2019-06-20 12:13:44 -0400

    main layout

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+// const index = require('./views/index');
+const layout = require('.views/layout')
 app.use(morgan('dev'));
 app.use(express.static(__dirname + 'public'));
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send(layout(""));
 })
+
 const PORT = 1000;
 app.listen(PORT, () => {
   console.log('App listening');
