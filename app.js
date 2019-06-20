da753eadaeaf9523caf98294c402072edad77c05
Sagar Vadalia <sv344@cornell.edu>
2019-06-20 12:33:24 -0400

    database running

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 // const index = require('./views/index');
-const layout = require('.views/layout')
+const layout = require('./views/layout')
+const { db } = require('./models');
 app.use(morgan('dev'));
 app.use(express.static(__dirname + 'public'));
 app.get('/', (req, res) => {
   res.send(layout(""));
+})
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
 })
 
 const PORT = 1000;
